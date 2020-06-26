163901b7fa3617831ed3473124e3bfd92a0259b9
西岡克弥 <nishikatsu@katsuya.local>
2020-06-26 23:53:10 +0900

    攻撃と必殺技

--- main.rb
+++ main.rb
@@ -1,48 +1,91 @@
 class Brave
-  # 複数の値を同時に指定できる
   attr_reader :name, :offense, :defense
-  # セッターゲッターを一括定義
   attr_accessor :hp
 
-  # new演算子から渡された引数を受け取る**paramsを設定でハッシュ値しか受け付けないようにする
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+  end
+
+  def attack(monster)
+
+    puts "#{@name}の攻撃"
+
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺技"
+      damage = special_attack
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
+    monster.hp -= damage
+
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
   end
 
 end
 
 class Monster
-  attr_reader :name, :offense,  :defense
-  attr_accessor :hp
+  attr_reader :offense,  :defense
+  attr_accessor :hp, :name
+
+  TRANSFORM = 1.5
+
+  CALC_HALF_HP =0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
   end
 
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+    def transform
+      transform_name = "ドラゴン"
+
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に返信した
+      EOS
+
+      @offense *= POWER_UP_RATE
+
+      @name = transform_name
+    end
 end
 
-# 勇者クラスをインスタンス化
 brave = Brave.new( name: "クラウド", hp: 500, offense: 150, defense: 100)
-# brave2 = Brave.new("ティファ", 300, 200, 80)
-# brave3 = Brave.new("バレット", 700, 30, 200)
-monster = Monster.new(name:"セフィロス", hp: 1000, offense: 100, defense: 100)
+monster = Monster.new( name: "セフィロス", hp: 250, offense: 200, defense: 100)
 
-# 値を取り出す
-puts <<~TEXT
-名前:#{brave.name}
-HP:#{brave.hp}
-攻撃:#{brave.offense}
-防御:#{brave.defense}
-TEXT
-
-# hpにダメージを与える処理
-brave.hp -= 30
-
-# ダメージに関するメッセージ
-puts "#{brave.name}はダメージを受けた！残りHPは#{brave.hp}だ"
+brave.attack(monster)
+monster.attack(brave)
