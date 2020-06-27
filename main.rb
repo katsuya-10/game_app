832a5dfea005c4a5ed5bf336403b175ca78744a8
西岡克弥 <nishikatsu@katsuya.local>
2020-06-28 05:40:05 +0900

    完成

--- main.rb
+++ main.rb
@@ -8,3 +8,6 @@ brave = Brave.new( name: "クラウド", hp: 500, offense: 200, defense: 100)
 monster = Monster.new( name: "セフィロス", hp: 500, offense: 200, defense: 100)
 
 games_controller.battle(brave: brave, monster: monster)
+
+EXP_CONSTANT = 2
+GIL_CONSTANT = 3
