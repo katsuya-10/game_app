88e19e59127b1f7f3842e0cad7ad2c5732815c27
西岡克弥 <nishikatsu@katsuya.local>
2020-06-28 03:13:03 +0900

    見やすく

--- games_controller.rb
+++ games_controller.rb
@@ -1,42 +1,50 @@
 class GamesController
-
   EXP_CONSTANT = 2
   GIL_CONSTANT = 3
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_characters(params)
 
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @brave.attack(monster)
+      break if battle_end?
+      @monster.attack(brave)
+      break if battle_end?
     end
 
-    if battle_result(brave)
-      result = colculate_of_exp_and_gil(monster)
-      puts "#{brave.name}は戦いに勝った"
-      puts "#{result[:exp]}pの経験値と#{result[:gil]}ギルを獲得"
-    else
-      puts "#{brave.name}は倒れた"
-      puts "全滅した"
-    end
+    battle_judgment
   end
 
   private
 
-    def battle_end?(character)
-      character.hp <= 0
+    def build_characters(**params)
+      @brave = params[:brave]
+      @monster = params[:monster]
     end
 
-    def battle_result(brave)
-      brave.hp > 0
+    def battle_end?
+      @brave.hp <= 0 || @monster.hp <= 0
     end
 
-    def colculate_of_exp_and_gil(monster)
-      exp = (monster.offense + monster.defense) * EXP_CONSTANT
-      gil = (monster.offense + monster.defense) * GIL_CONSTANT
+    def brave_win?
+      @brave.hp > 0
+    end
+
+    def battle_judgment
+
+      if brave_win?
+        result = colculate_of_exp_and_gil
+        puts "#{@brave.name}は戦いに勝った"
+        puts "#{result[:exp]}pの経験値と#{result[:gil]}ギルを獲得"
+      else
+        puts "#{@brave.name}は倒れた"
+        puts "全滅した"
+      end
+    end
+
+    def calculate_of_exp_and_gil
+      exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+      gil = (@monster.offense + @monster.defense) * GIL_CONSTANT
       result = {exp: exp, gil: gil}
 
       result
